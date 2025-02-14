86fb75d4088531a8248588ba04a878a151df44b0
wisuwat <wisuwatpond01@gmail.com>
2025-02-14 13:44:32 +0700

    Button Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './App.css'
 import { Button } from "antd";
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  function handleClick (e) {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+  }
 
   return (
     <>
-    <Button color='pink' variant='filled'>My First Button</Button>
+    <Button color='pink' variant='filled' loading={loading} onClick={handleClick}>My First Button</Button>
     </>
   )
 }
